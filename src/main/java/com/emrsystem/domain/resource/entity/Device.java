3f14b1d5ca1b7512be8173728149bf26b3c674f2
RosieOh <dhxogns920@gmail.com>
2025-10-06 13:35:37 +0900

    FEAT : 병원 장비 도메인 추가

---
+++ src/main/java/com/emrsystem/domain/resource/entity/Device.java
@@ -0,0 +1,38 @@
+package com.emrsystem.domain.resource.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "device")
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "device_id")
+    private Long deviceId;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String code;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    private Device(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public static Device of(String code, String name) { return new Device(code, name); }
+}
+
+
