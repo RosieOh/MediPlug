d7f881b6b28c5bb52f9b000d259a5478f3ad1a6b
RosieOh <dhxogns920@gmail.com>
2025-10-06 22:05:25 +0900

    FEAT : 병실 도메인 추가

---
+++ src/main/java/com/emrsystem/domain/resource/entity/Room.java
@@ -0,0 +1,45 @@
+package com.emrsystem.domain.resource.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long roomId;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String code;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    private Room(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public static Room of(String code, String name) { return new Room(code, name); }
+
+    public void update(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public Long getId() { return this.roomId; }
+}
+
+
