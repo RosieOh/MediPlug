ecaea6f61134d8bcce19b5dcaabc2fdbe840aee4
RosieOh <dhxogns920@gmail.com>
2025-10-06 22:16:41 +0900

    FEAT : 예약 상태 객체 추가

---
+++ src/main/java/com/emrsystem/domain/appointment/enums/AppointmentStatus.java
@@ -0,0 +1,13 @@
+package com.emrsystem.domain.appointment.enums;
+
+public enum AppointmentStatus {
+    SCHEDULED,
+    CONFIRMED,
+    ARRIVED,
+    IN_PROGRESS,
+    COMPLETED,
+    NO_SHOW,
+    CANCELLED
+}
+
+
