eacf905fac346d990f93b10cdfc9b65968ae1fd5
RosieOh <dhxogns920@gmail.com>
2025-10-06 22:14:37 +0900

    FEAT : 사용자 계정 도메인 추가

---
+++ src/main/java/com/emrsystem/domain/user/entity/UserAccount.java
@@ -0,0 +1,51 @@
+package com.emrsystem.domain.user.entity;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "user_account")
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role", length = 50)
+    private Set<String> roles = new HashSet<>();
+
+    private UserAccount(String username, String password, Set<String> roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public static UserAccount create(String username, String encodedPassword, Set<String> roles) {
+        return new UserAccount(username, encodedPassword, roles);
+    }
+}
+
+
