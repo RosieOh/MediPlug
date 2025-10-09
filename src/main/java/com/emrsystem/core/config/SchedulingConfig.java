1f3f7c88cdc3fa96158a18a130d025277faab6d0
RosieOh <dhxogns920@gmail.com>
2025-10-09 19:08:10 +0900

    FEAT : 스케줄링 설정 로직 추가

---
+++ src/main/java/com/emrsystem/core/config/SchedulingConfig.java
@@ -0,0 +1,11 @@
+package com.emrsystem.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
+
+
